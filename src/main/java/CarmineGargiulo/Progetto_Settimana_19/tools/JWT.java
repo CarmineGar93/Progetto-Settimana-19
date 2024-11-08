2759764f470500e3a6dc4971c0fdd2f9f405313f
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-08 12:21:13 +0100

    jwt tool created for tokens handling

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_19/tools/JWT.java
@@ -0,0 +1,37 @@
+package CarmineGargiulo.Progetto_Settimana_19.tools;
+
+import CarmineGargiulo.Progetto_Settimana_19.entities.User;
+import CarmineGargiulo.Progetto_Settimana_19.exceptions.UnauthoriedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWT {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .subject(String.valueOf(user.getUserId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception e) {
+            throw new UnauthoriedException("Invalid token. Please re-do login");
+        }
+    }
+
+    public String extractUserIdFromToken(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+}
