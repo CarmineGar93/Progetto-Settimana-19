cb6dbbec5c17a5eb9d08dd6b6a6d2a321b9b38e2
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-08 10:30:04 +0100

    Initialized spring boot application and database parameters

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_19/ProgettoSettimana19Application.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.Progetto_Settimana_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana19Application.class, args);
+	}
+
+}
---
+++ src/test/java/CarmineGargiulo/Progetto_Settimana_19/ProgettoSettimana19ApplicationTests.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.Progetto_Settimana_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
